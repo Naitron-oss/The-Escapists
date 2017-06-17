199435240f08e3b5e61a019bbd43bb3e9f1aa0ae
Ravi Wu <raviwu@gmail.com>
2017-06-17 09:47:29 +0800

    Add Error Handle and Index Page

--- escapist.js
+++ escapist.js
@@ -7,10 +7,23 @@ app.disable('x-powered-by');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 const studios = require('./datas/studios');
 
-app.get('/', (req, res) => {
+app.get('/api/studios', (req, res) => {
   res.json(studios);
+});
+
+// 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+  res.status(404).send('Page not found');
+});
+
+// 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
 });
 
 app.listen(app.get('port'), () => {
